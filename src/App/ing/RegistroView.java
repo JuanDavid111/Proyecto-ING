b5fc006fdb7e1bfcf2f7bf95feb0cea5e701e0c4
Juan <juandavid2002111@gmail.com>
2024-07-28 17:35:58 -0400

    buscar p

---
+++ src/App/ing/RegistroView.java
@@ -0,0 +1,25 @@
+package App.ing;
+import vista.UI_Login;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+/**
+ *
+ * @author emili
+ */
+public class RegistroView {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        UI_Login inicial = new UI_Login();
+        inicial.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
